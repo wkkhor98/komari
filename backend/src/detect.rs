c123e0065acc922532bab4bf63e71a6aa058e373
weikeat <khorwe@hotmail.com>
2026-05-12 13:32:14 +0800

    fxi(): add missing import

--- backend/src/detect.rs
+++ backend/src/detect.rs
@@ -12,6 +12,7 @@ use log::{debug, error, info};
 use mockall::automock;
 use opencv::{
     boxed_ref::BoxedRef,
+    prelude::VectorToVec,
     core::{
         BORDER_CONSTANT, CMP_EQ, CMP_GT, CV_8U, CV_32FC3, CV_32S, Mat, MatExprTraitConst, MatTrait,
         MatTraitConst, MatTraitConstManual, ModifyInplace, Point, Range, Rect, Scalar, Size,
